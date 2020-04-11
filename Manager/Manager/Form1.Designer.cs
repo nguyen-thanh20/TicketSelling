7e5a9ab29aa89347b926f929973fbbc5b3cb9740
Công Dương <53830837+chinchyhaga@users.noreply.github.com>
2020-04-11 21:04:33 +0700

    Add files via upload

---
+++ Manager/Manager/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subpanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.statusbutton = new System.Windows.Forms.Button();
+            this.statuspanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.searchbutton = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.toolbutton = new System.Windows.Forms.Button();
+            this.toolpanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.formpanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.subpanel.SuspendLayout();
+            this.statuspanel.SuspendLayout();
+            this.toolpanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // subpanel
+            // 
+            this.subpanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.subpanel.Controls.Add(this.panel2);
+            this.subpanel.Controls.Add(this.statusbutton);
+            this.subpanel.Controls.Add(this.statuspanel);
+            this.subpanel.Controls.Add(this.toolbutton);
+            this.subpanel.Controls.Add(this.toolpanel);
+            this.subpanel.Controls.Add(this.button15);
+            this.subpanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.subpanel.Location = new System.Drawing.Point(0, 0);
+            this.subpanel.Name = "subpanel";
+            this.subpanel.Size = new System.Drawing.Size(250, 588);
+            this.subpanel.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(250, 109);
+            this.panel2.TabIndex = 0;
+            // 
+            // statusbutton
+            // 
+            this.statusbutton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statusbutton.FlatAppearance.BorderSize = 0;
+            this.statusbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.statusbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusbutton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.statusbutton.Location = new System.Drawing.Point(0, 109);
+            this.statusbutton.Margin = new System.Windows.Forms.Padding(0);
+            this.statusbutton.Name = "statusbutton";
+            this.statusbutton.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.statusbutton.Size = new System.Drawing.Size(250, 40);
+            this.statusbutton.TabIndex = 1;
+            this.statusbutton.Text = "STATUS";
+            this.statusbutton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.statusbutton.UseVisualStyleBackColor = true;
+            this.statusbutton.Click += new System.EventHandler(this.statusbutton_Click);
+            // 
+            // statuspanel
+            // 
+            this.statuspanel.Controls.Add(this.button9);
+            this.statuspanel.Controls.Add(this.button10);
+            this.statuspanel.Controls.Add(this.button12);
+            this.statuspanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.statuspanel.Location = new System.Drawing.Point(0, 149);
+            this.statuspanel.Margin = new System.Windows.Forms.Padding(0);
+            this.statuspanel.Name = "statuspanel";
+            this.statuspanel.Size = new System.Drawing.Size(250, 120);
+            this.statuspanel.TabIndex = 2;
+            // 
+            // searchbutton
+            // 
+            this.searchbutton.FlatAppearance.BorderSize = 0;
+            this.searchbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.searchbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchbutton.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.searchbutton.Location = new System.Drawing.Point(0, 0);
+            this.searchbutton.Margin = new System.Windows.Forms.Padding(0);
+            this.searchbutton.Name = "searchbutton";
+            this.searchbutton.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.searchbutton.Size = new System.Drawing.Size(250, 40);
+            this.searchbutton.TabIndex = 0;
+            this.searchbutton.Text = "Manage";
+            this.searchbutton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.searchbutton.UseVisualStyleBackColor = true;
+            this.searchbutton.Click += new System.EventHandler(this.searchbutton_Click);
+            // 
+            // button9
+            // 
+            this.button9.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button9.Location = new System.Drawing.Point(0, 0);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button9.Size = new System.Drawing.Size(250, 40);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "Active";
+            this.button9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button10.Location = new System.Drawing.Point(0, 40);
+            this.button10.Margin = new System.Windows.Forms.Padding(0);
+            this.button10.Name = "button10";
+            this.button10.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button10.Size = new System.Drawing.Size(250, 40);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "Outdated";
+            this.button10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // toolbutton
+            // 
+            this.toolbutton.FlatAppearance.BorderSize = 0;
+            this.toolbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toolbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolbutton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toolbutton.Location = new System.Drawing.Point(0, 269);
+            this.toolbutton.Margin = new System.Windows.Forms.Padding(0);
+            this.toolbutton.Name = "toolbutton";
+            this.toolbutton.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.toolbutton.Size = new System.Drawing.Size(250, 40);
+            this.toolbutton.TabIndex = 1;
+            this.toolbutton.Text = "TOOL";
+            this.toolbutton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolbutton.UseVisualStyleBackColor = true;
+            this.toolbutton.Click += new System.EventHandler(this.toolbutton_Click);
+            // 
+            // toolpanel
+            // 
+            this.toolpanel.Controls.Add(this.searchbutton);
+            this.toolpanel.Controls.Add(this.button11);
+            this.toolpanel.Controls.Add(this.button13);
+            this.toolpanel.Controls.Add(this.button14);
+            this.toolpanel.Location = new System.Drawing.Point(0, 309);
+            this.toolpanel.Margin = new System.Windows.Forms.Padding(0);
+            this.toolpanel.Name = "toolpanel";
+            this.toolpanel.Size = new System.Drawing.Size(250, 160);
+            this.toolpanel.TabIndex = 3;
+            // 
+            // button12
+            // 
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button12.Location = new System.Drawing.Point(0, 80);
+            this.button12.Margin = new System.Windows.Forms.Padding(0);
+            this.button12.Name = "button12";
+            this.button12.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button12.Size = new System.Drawing.Size(250, 40);
+            this.button12.TabIndex = 1;
+            this.button12.Text = "Graph";
+            this.button12.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button11.Location = new System.Drawing.Point(0, 40);
+            this.button11.Margin = new System.Windows.Forms.Padding(0);
+            this.button11.Name = "button11";
+            this.button11.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button11.Size = new System.Drawing.Size(250, 40);
+            this.button11.TabIndex = 0;
+            this.button11.Text = "button11";
+            this.button11.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button13
+            // 
+            this.button13.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button13.FlatAppearance.BorderSize = 0;
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button13.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button13.Location = new System.Drawing.Point(0, 80);
+            this.button13.Margin = new System.Windows.Forms.Padding(0);
+            this.button13.Name = "button13";
+            this.button13.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button13.Size = new System.Drawing.Size(250, 40);
+            this.button13.TabIndex = 2;
+            this.button13.Text = "button12";
+            this.button13.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button14.FlatAppearance.BorderSize = 0;
+            this.button14.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button14.Location = new System.Drawing.Point(0, 120);
+            this.button14.Margin = new System.Windows.Forms.Padding(0);
+            this.button14.Name = "button14";
+            this.button14.Padding = new System.Windows.Forms.Padding(30, 0, 0, 0);
+            this.button14.Size = new System.Drawing.Size(250, 40);
+            this.button14.TabIndex = 3;
+            this.button14.Text = "Guest List";
+            this.button14.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button15.FlatAppearance.BorderSize = 0;
+            this.button15.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button15.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button15.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button15.Location = new System.Drawing.Point(0, 469);
+            this.button15.Margin = new System.Windows.Forms.Padding(0);
+            this.button15.Name = "button15";
+            this.button15.Padding = new System.Windows.Forms.Padding(10, 0, 0, 0);
+            this.button15.Size = new System.Drawing.Size(250, 40);
+            this.button15.TabIndex = 1;
+            this.button15.Text = "HELP";
+            this.button15.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // formpanel
+            // 
+            this.formpanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.formpanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formpanel.Location = new System.Drawing.Point(250, 0);
+            this.formpanel.Name = "formpanel";
+            this.formpanel.Size = new System.Drawing.Size(806, 588);
+            this.formpanel.TabIndex = 0;
+            this.formpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.formpanel_Paint);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1056, 588);
+            this.Controls.Add(this.formpanel);
+            this.Controls.Add(this.subpanel);
+            this.Name = "Form1";
+            this.subpanel.ResumeLayout(false);
+            this.statuspanel.ResumeLayout(false);
+            this.toolpanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button Panellogo;
+        private System.Windows.Forms.Panel statussub;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel toolsub;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel subpanel;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button statusbutton;
+        private System.Windows.Forms.FlowLayoutPanel statuspanel;
+        private System.Windows.Forms.Button searchbutton;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button toolbutton;
+        private System.Windows.Forms.FlowLayoutPanel toolpanel;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.FlowLayoutPanel formpanel;
+        private System.Windows.Forms.Button button14;
+    }
+}
+
